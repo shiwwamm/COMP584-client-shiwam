017c767ab34d138568246986d2db2cf9181a3d22
shiwwamm <vermashivams1510@gmail.com>
2024-09-20 15:49:59 -0700

    Added country,city,weaather component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+
 
 @Component({
   selector: 'app-root',
@@ -18,27 +13,8 @@ interface WeatherForecast {
 })
 
 
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('http://localhost:5283/weatherforecast').subscribe(
-      (result) => {``
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  title = 'angularapp1.client';
+export class AppComponent {
+  
 }
 // export class AppComponent {
 //   title = 'clientApplication';
